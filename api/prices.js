e5ac26b8fb72351d4c309dc7cf5fb53ab21377b3
ksycheng <ksycheng@gmail.com>
2026-05-16 18:03:43 -0400

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     const fullPrompt = (system ? system + "\n\n" : "") + prompt;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + apiKey,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
